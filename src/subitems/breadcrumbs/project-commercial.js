314c4e98d51d267a952780ac63a735e041514b4a
Anzela <anzelakukjan.work@gmail.com>
2023-07-14 14:47:25 +0300

    Add breadcrumbs

---
+++ src/subitems/breadcrumbs/project-commercial.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import { Link } from "gatsby"
+import * as styles from "./breadcrumbs.module.scss"
+
+
+export const BreadcrumbsProject = () => {
+  return (
+    <ul className={styles.breadcrumbs + " " + styles.breadcrumbs_black}>
+      <li className={styles.breadcrumbs_li}>
+        <Link to="/portfolio" className={styles.breadcrumbs_link}>Портфолио</Link>
+      </li>
+      <li className={styles.breadcrumbs_li}>
+        <Link to="/portfolio" className={styles.breadcrumbs_link}>Коммерческие объекты</Link>
+      </li>
+    </ul>
+  )
+}
